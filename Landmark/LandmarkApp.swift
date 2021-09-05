fbb56b4a724b2823a0b98d2e00b807b5f6bcf942
Christian Garrovillo <6688713+cgarrovillo@users.noreply.github.com>
2021-09-05 10:17:38 -0600

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Christian Garrovillo on 2021-09-05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
