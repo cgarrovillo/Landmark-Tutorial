80f5df410073bdcfc3b13d80e221921a73c67928
Christian Garrovillo <6688713+cgarrovillo@users.noreply.github.com>
2021-09-07 15:25:40 -0600

    SwiftUI tutorials

---
+++ Landmark/Views/Helpers/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmark
+//
+//  Created by Christian Garrovillo on 2021-09-05.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
